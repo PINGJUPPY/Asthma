55007f62138e2932adcab9e9741bf20fae08e648
PINGJUPPY <ping.khong@gmail.com>
2026-01-12 09:54:29 +0700

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,5 +1,7 @@
 importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js');
+
+// ⚠️ Config ชุดเดียวกับใน script.js
 firebase.initializeApp({
     apiKey: "AIzaSyADXEA4Hs_WJDXVxfsGHLyPytTVypZqd6U",
     authDomain: "asthmaalert-903b7.firebaseapp.com",
@@ -9,9 +11,18 @@ firebase.initializeApp({
     appId: "1:123117910600:web:d90af1677fa7e04b50767d",
     measurementId: "G-YBLJPHJXK3"
 });
+
 const messaging = firebase.messaging();
+
+// จัดการการแสดงผลเมื่อได้รับแจ้งเตือนตอนปิดหน้าจอ (Background)
 messaging.onBackgroundMessage((payload) => {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  
   const notificationTitle = payload.notification.title;
-  const notificationOptions = { body: payload.notification.body, icon: 'https://cdn-icons-png.flaticon.com/512/3063/3063822.png' };
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: 'https://cdn-icons-png.flaticon.com/512/3063/3063822.png' // ไอคอนแอพ
+  };
+
   self.registration.showNotification(notificationTitle, notificationOptions);
 });
